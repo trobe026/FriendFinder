fdbb81499115c03fdec26928fe0a0950ee9b3454
Travis Roberts <travis.roberts2@gmail.com>
2018-02-05 20:23:45 -0600

    api routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 var friendsList = [{
   "name":"Ahmed",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Shabaz_Ahmed_Senior.jpg/1200px-Shabaz_Ahmed_Senior.jpg",
   "scores":[
       '5',
       '1',
@@ -13,6 +13,54 @@ var friendsList = [{
       '4',
       '1'
     ]
+},
+{
+  "name": "Ethel",
+  "photo": "https://media0.giphy.com/media/jA8TT03Sj2pXO/source.gif",
+  "scores": [
+    '2',
+    '3',
+    '1',
+    '2',
+    '3',
+    '1',
+    '4',
+    '4',
+    '5',
+    '1'
+  ]
+},
+// {
+//   "name": "Scott",
+//   "photo": "https://ledonfrancois.files.wordpress.com/2009/06/blog451.jpg",
+//   "scores": [
+//     '2',
+//     '3',
+//     '2',
+//     '3',
+//     '2',
+//     '3',
+//     '2',
+//     '3',
+//     '2',
+//     '3'
+//   ]
+// },
+{
+  "name": "A Calculator",
+  "photo": "https://upload.wikimedia.org/wikipedia/commons/4/4d/TI-83.png",
+  "scores":[
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1',
+    '1'
+  ]
 }];
 
 module.exports = friendsList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var friendsList = require("../data/friends");
+var match;
 
 module.exports = function(app) {
 
@@ -6,11 +7,70 @@ module.exports = function(app) {
     res.json(friendsList);
   });
 
-  app.post("/api/friends", function(req, res) {
-    console.log(req.body);
-    console.log(req.body.scores[2]);
-    friendsList.push(req.body);
-    res.json(true);
+  app.get("/api/match", function(req, res) {
+    res.json(match);
   });
 
+  app.post("/api/friends", function(req, res) {
+    // console.log(req.body);
+    friendsList.push(req.body);
+    var currentUser = friendsList[friendsList.length - 1];
+    var diffs = [];
+    // console.log(currentUser);
+    for (var i = 0; i < friendsList.length - 1; i++) {
+      var diff = 0;
+      for (var j = 0; j < friendsList[i].scores.length - 1; j++) {
+
+        // console.log(friendsList[i].scores);
+        diff += Math.abs(friendsList[i].scores[j] - currentUser.scores[j]);
+        // console.log(diff);
+        // console.log(currentUser);
+        // totalDiff += parseInt(diff);
+        // console.log(currentUser.scores[j])
+        // console.log(friendsList[i].scores[j])
+      }
+      // console.log(friendsList.length);
+      diffs.push(diff);
+
+      // var min = diffs[0];
+      // var minIndex = 0;
+      // for (var i = 1; i < diffs.length; i++) {
+      //   if (diffs[i] < min) {
+      //     minIndex = i;
+      //     min = diffs[i];
+      //   }
+      // }
+      // console.log(diffs);
+      // console.log(i);
+      // console.log(friendsList[i].scores);
+      // console.log(currentUser.scores);
+
+
+    }
+    match = [friendsList[indexOfmin(diffs)].name, friendsList[indexOfmin(diffs)].photo];
+    console.log(friendsList[indexOfmin(diffs)].name);
+    console.log(friendsList[indexOfmin(diffs)].photo);
+    // console.log(friendsList[minIndex]);
+    // console.log("_____________________")
+    // console.log(friendsList);
+  });
+
+}
+// function to return lowest
+function indexOfmin(arr) {
+    if (arr.length === 0) {
+        return -1;
+    }
+
+    var min = arr[0];
+    var minIndex = 0;
+
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            minIndex = i;
+            min = arr[i];
+        }
+    }
+
+    return minIndex;
 }
